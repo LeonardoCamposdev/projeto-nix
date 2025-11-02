a1e759f3ffe64d1415d9824bd7a870a3ed6c59e1
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-11-02 13:27:53 -0300

    upp

--- js/modules/trocar-iframe.js
+++ js/modules/trocar-iframe.js
@@ -1,5 +1,10 @@
 export function initTrocarIframe1() {
   const video1 = document.querySelector('[data-projeto="1"]');
+  const video2 = document.querySelector('[data-projeto="2"]');
+  const video3 = document.querySelector('[data-projeto="3"]');
+  const video4 = document.querySelector('[data-projeto="4"]');
+  video1.classList.add("ativo-teste");
+
   const iframeVideo = document.querySelector('[data-iframe="video"]');
   const spanTitulo = document.querySelector(".spanTitulo-projeto");
   const spanDescricao = document.querySelector(".spanDescricao-projeto");
@@ -7,7 +12,10 @@ export function initTrocarIframe1() {
   function handleClick() {
     const novoVideo = "https://www.youtube.com/embed/JynGuQx4a1Y?start=3";
     iframeVideo.setAttribute("src", novoVideo);
-    video1.classList.add('ativo-teste')
+    video1.classList.add("ativo-teste");
+    video2.classList.remove("ativo-teste");
+    video3.classList.remove("ativo-teste");
+    video4.classList.remove("ativo-teste");
 
     spanTitulo.innerText = "Projeto Um";
     spanDescricao.innerText = "SEO e Marketing";
@@ -17,7 +25,11 @@ export function initTrocarIframe1() {
 }
 
 export function initTrocarIframe2() {
+  const video1 = document.querySelector('[data-projeto="1"]');
   const video2 = document.querySelector('[data-projeto="2"]');
+  const video3 = document.querySelector('[data-projeto="3"]');
+  const video4 = document.querySelector('[data-projeto="4"]');
+
   const iframeVideo = document.querySelector('[data-iframe="video"]');
   const spanTitulo = document.querySelector(".spanTitulo-projeto");
   const spanDescricao = document.querySelector(".spanDescricao-projeto");
@@ -25,6 +37,10 @@ export function initTrocarIframe2() {
   function handleClick() {
     const novoVideo = "https://www.youtube.com/embed/RdJBSFpcO4M?start=1";
     iframeVideo.setAttribute("src", novoVideo);
+    video1.classList.remove("ativo-teste");
+    video2.classList.add("ativo-teste");
+    video3.classList.remove("ativo-teste");
+    video4.classList.remove("ativo-teste");
 
     spanTitulo.innerText = "Projeto Dois";
     spanDescricao.innerText = "Publicidade e Marketing";
@@ -34,7 +50,11 @@ export function initTrocarIframe2() {
 }
 
 export function initTrocarIframe3() {
+  const video1 = document.querySelector('[data-projeto="1"]');
+  const video2 = document.querySelector('[data-projeto="2"]');
   const video3 = document.querySelector('[data-projeto="3"]');
+  const video4 = document.querySelector('[data-projeto="4"]');
+
   const iframeVideo = document.querySelector('[data-iframe="video"]');
   const spanTitulo = document.querySelector(".spanTitulo-projeto");
   const spanDescricao = document.querySelector(".spanDescricao-projeto");
@@ -43,6 +63,10 @@ export function initTrocarIframe3() {
     // novo vídeo que você quer carregar
     const novoVideo = "https://www.youtube.com/embed/ZlfAjbQiL78?start=11";
     iframeVideo.setAttribute("src", novoVideo);
+    video1.classList.remove("ativo-teste");
+    video2.classList.remove("ativo-teste");
+    video3.classList.add("ativo-teste");
+    video4.classList.remove("ativo-teste");
 
     spanTitulo.innerText = "Projeto Três";
     spanDescricao.innerText = "Marketing Digital";
@@ -52,15 +76,22 @@ export function initTrocarIframe3() {
 }
 
 export function initTrocarIframe4() {
+  const video1 = document.querySelector('[data-projeto="1"]');
+  const video2 = document.querySelector('[data-projeto="2"]');
+  const video3 = document.querySelector('[data-projeto="3"]');
   const video4 = document.querySelector('[data-projeto="4"]');
+
   const iframeVideo = document.querySelector('[data-iframe="video"]');
   const spanTitulo = document.querySelector(".spanTitulo-projeto");
   const spanDescricao = document.querySelector(".spanDescricao-projeto");
 
-  function handleClick(event) {
-    // novo vídeo que você quer carregar
+  function handleClick() {
     const novoVideo = "https://www.youtube.com/embed/mx1WseE7-0Y";
     iframeVideo.setAttribute("src", novoVideo);
+    video1.classList.remove("ativo-teste");
+    video2.classList.remove("ativo-teste");
+    video3.classList.remove("ativo-teste");
+    video4.classList.add("ativo-teste");
 
     spanTitulo.innerText = "Projeto Quatro";
     spanDescricao.innerText = "SEO e Publicidade";
