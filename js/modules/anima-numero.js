872838712915b4955bb16aec0ce6bad1524951e2
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-11-03 16:39:27 -0300

    up

--- js/modules/anima-numero.js
+++ js/modules/anima-numero.js
@@ -1,20 +1,34 @@
 export default function initAnimaNumeros() {
+    function animaNumeros(){
+    const numeros = document.querySelectorAll('[data-numero]');
+    numeros.forEach((numero)=>{
+        const total = +numero.innerText;
+        const incremento = Math.floor(total / 100);
+
+        let start = 0;
+        const timer = setInterval(()=>{
+            start = start + incremento;
+            numero.innerText = start;
+
+            if(start > total){
+                numero.innerText = total;
+                clearInterval(timer);
+            }
+        }, 25, Math.random());
+    })
 }
 
-const numeros = document.querySelectorAll('[data-numero]');
 
-numeros.forEach((numero) =>{
-    const total = +numero.innerText;
-    const incremento = Math.floor(total /100);
+function handleOberser(mutation){
+    if(mutation[0].target.classList.contains('ativo')){
+        observer.disconnect();
+        animaNumeros();
+    }
+}
+const observerTarget = document.querySelector('[data-observer]')
+const observer = new MutationObserver(handleOberser);
 
-    let start = 0;
-    const timer = setInterval(()=>{
-        start = start + incremento;
-        numero.innerText = start;
-        
-        if(start > total){
-            numero.innerText = total;
-            clearInterval(timer);
-        }
-    }, 25, Math.random());
-})
+observer.observe(observerTarget, {attributes: true});
+
+}
+
